2c5975131ee7763ac880c2cdf82f9b57bd496171
mariana <marianasalma11@gmail.com>
2022-11-10 08:26:56 +0700

    finish tugas 7

---
+++ tugas7/README.md
@@ -0,0 +1,23 @@
+# Tugas 7
+
+**Jelaskan apa yang dimaksud dengan stateless widget dan stateful widget dan jelaskan perbedaan dari keduanya.**
+Stateless widget merupakan widget yang tidak berubah pada interface, seperti Text atau Icon. Stateful widget merupkan widget yang bisa berubah jika user berinteraksi dengan widget tersebut.
+
+**Sebutkan widget apa saja yang kamu pakai di proyek kali ini dan jelaskan fungsinya.**
+- Text widget untuk menampilkan dan styling text.
+- Center widget untuk menampilkan child elementsnya secara center.
+- FloatingActionButton yang memanggil sebuah fungsi jika dipencet.
+- Icon widget yang menampilkan sebuah icon
+
+**Apa fungsi dari setState()? Jelaskan variabel apa saja yang dapat terdampak dengan fungsi tersebut.**
+Memanggil setState menginformasikan Flutter bahwa state dari objek telah berubah dan mempengaruhi interface sehingga harus melakukan build ulang agar perubahan tersebut terlihat. Variabel _counter, _text, dan genap.
+
+**Jelaskan perbedaan antara const dengan final.**
+Objek const harus diassign ke sebuah data yang sudah ada pada saat compiling, sementara final dapat digunakan jika data diambil saat run-time. Misal, datetime tidak dapat digunakan untuk variabel const karena nilai tersebut dikalkulasikan saat run-time.
+
+**Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas.**
+- Melakukan flutter create untuk melakukan proyek baru, membuka lib/main.dart
+- Inisialisasi variabel _counter, _text, dan genap.
+- Membuat fungsi increment dan decrement untuk counter.
+- Menambahkan Text dan color menggunakan TextStyle untuk warna merah jika genap dan biru jika ganjil.
+- Menambahkan FloatingActionButton yang menyambung ke fungsi increment/decrement jika onPressed.
---
+++ tugas7/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Program Counter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Program Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _text = 'GENAP';
+  bool genap = true;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if (_counter % 2 == 0) {
+        _text = 'GENAP';
+        genap=true;
+      } else {
+        _text = 'GANJIL';
+        genap=false;
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      if (_counter % 2 == 0) {
+        _text = 'GENAP';
+        genap=true;
+      } else {
+        _text = 'GANJIL';
+        genap=false;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_text, style: TextStyle(color: genap==true?Colors.red:Colors.blue),),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Center(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+                const SizedBox(width: 100),
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
